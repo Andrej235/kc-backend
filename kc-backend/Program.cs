1593b396bcee7947bcd964eafa669c2b4232e401
Andrej235 <andrej.nenadic06@gmail.com>
2024-11-14 12:37:59 +0100

    Setup DataContext as a service in program.cs

--- kc-backend/Program.cs
+++ kc-backend/Program.cs
@@ -1,4 +1,7 @@
 
+using kc_backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace kc_backend
 {
     public class Program
@@ -6,6 +9,13 @@ namespace kc_backend
         public static void Main(string[] args)
         {
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            ConfigurationManager configuration = builder.Configuration;
+            _ = builder.Services.AddSingleton(configuration);
+            _ = builder.Services.AddDbContext<DataContext>(x =>
+            {
+                _ = x.UseSqlServer(configuration.GetConnectionString("SQLConnectionString"));
+                _ = x.EnableSensitiveDataLogging(); //TODO-PROD: remove in production
+            });
 
             _ = builder.Services.AddControllers();
             _ = builder.Services.AddOpenApi();
